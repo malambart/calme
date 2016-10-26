adc69d2a7951d9c47276c8e750cf76a06a614118
Francis Lafortune <francislafort@gmail.com>
2016-10-26 14:00:59 -0400

    ajout formulaire modal

--- resources/views/mesures/create.blade.php
+++ resources/views/mesures/create.blade.php
@@ -3,6 +3,44 @@
 <h1>Dossier {{$dossier->id}} : ajouter un temps de mesure</h1>
 @endsection
 @section('body')
+<!-- Modal -->
+<div class="modal fade" id="ecoleForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Ajouter une école</h4>
+			</div>
+			<div class="modal-body">
+				<form class="bootstrap-modal-form clearfix" role="form" method="POST" action="{{ url('ecoles/create') }}">
+					{{ csrf_field() }}
+					<div class="form-group{{ $errors->has('nom') ? ' has-error' : '' }}">
+						<label for="nom" class="control-label">Nom de l'école</label>
+							<input id="nom" type="text" class="form-control" name="nom" value="{{ old('nom') }}" autofocus>
+							@if ($errors->has('nom'))
+							<p class="help-block">
+								<strong>{{ $errors->first('nom') }}</strong>
+							</p>
+							@endif
+					</div>
+					<div class="form-group{{ $errors->has('ville') ? ' has-error' : '' }}">
+						<label for="ville" class="control-label">Ville</label>
+							<input id="ville" type="text" class="form-control" name="ville" value="{{ old('ville') }}" autofocus>
+							@if ($errors->has('ville'))
+							<span class="help-block">
+								<strong>{{ $errors->first('ville') }}</strong>
+							</span>
+							@endif
+					</div>
+					<button type="submit" class="btn btn-primary pull-right">
+						Ajouter
+					</button>
+				</form>
+			</div>
+
+		</div>
+	</div>
+</div>
 <form class="form-horizontal" role="form" method="POST" action="{{ url('mesures/'.$dossier->id.'/create') }}">
 	{{ csrf_field() }}
 	<div class="form-group{{ $errors->has('prenom_ens') ? ' has-error' : '' }}">
@@ -29,19 +67,27 @@
 	</div>
 	<div class="form-group{{ $errors->has('ecole_id') ? ' has-error' : '' }}">
 		<label for="ecole_id" class="col-md-4 control-label">École</label>
-		<a class="btn btn-primary float-right" tabindex="-1" target="_blank" href="{{url('/ecoles/create')}}"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+		
 		<div class="col-md-6">
-			<select name="ecole_id" class="form-control">
-				<option value="">Veuillez choisir</option>
-				@foreach($ecoles as $ecole)
-				<option value="{{$ecole->id}}">{{$ecole->nom}}</option>
-				@endforeach
-			</select>
+			<div class="input-group">
+				<select name="ecole_id" class="form-control">
+					<option value="">Veuillez choisir</option>
+					@foreach($ecoles as $ecole)
+					<option value="{{$ecole->id}}">{{$ecole->nom}}</option>
+					@endforeach
+				</select>
+				<span class="input-group-btn">
+					<button tabindex="-1" type="button" class="btn btn-primary bootstrap-modal-form-open" data-toggle="modal" data-target="#ecoleForm">+</button>
+				</span>	
+				
+			</div>
+			
 			@if ($errors->has('ecole_id'))
 			<span class="help-block">
 				<strong>{{ $errors->first('ecole_id') }}</strong>
 			</span>
 			@endif
+			
 		</div>
 	</div>	
 	<div class="form-group{{ $errors->has('tel_ens') ? ' has-error' : '' }}">
