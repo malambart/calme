51aeeb167f419a72cf9d9c9cda1c83ba511a0b67
Francis Lafortune <francislafort@gmail.com>
2016-11-23 16:20:16 -0500

    ajout fonctionnalité date pour input:date pas supporte

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,11 @@ elixir(mix => {
     '../bower/moment/min/moment.min.js',
     '../bower/moment/locale/fr.js',
     '../bower/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js', 
-    '../bower/jquery-ui/jquery-ui.min.js' 
-
+    '../bower/jquery-ui/jquery-ui.min.js'
 ], 'public/js/vendor.js');
     mix.styles([
-        '../bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+        '../bower/eonasdPan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
         '../bower/jquery-ui/themes/base/jquery-ui.min.css',
         ],'public/css/vendor.css');
 });
-
+P
