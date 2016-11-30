82726f0aa6f727c68228fc00db3988908599e881
Francis Lafortune <francislafort@gmail.com>
2016-11-30 11:29:20 -0500

    correction mom classe ParentRep

--- app/Dossier.php
+++ app/Dossier.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Updater;
-use App\Parentrep;
+use App\ParentRep;
 use Carbon\Carbon;
 use Sofa\Eloquence\Eloquence;
 //use Illuminate\Support\Facades\Auth
