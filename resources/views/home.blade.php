2d58bca1e6533c24b1f4c232d2d825296b2daec7
Francis Lafortune <francislafort@gmail.com>
2016-12-16 10:34:29 -0500

    ajouté chart.js dans les dependances

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,44 @@
 @extends('layouts.row')
 @section('panel-heading')
-<h1>Rechercher un dossier</h1>
+    <h1>Rechercher un dossier</h1>
 @endsection
 @section('body')
-<form  role="form" method="POST" action="{{ url('recherche') }}">
-    {{ csrf_field() }}
-    <div class="form-group{{ $errors->has('recherche') ? ' has-error' : '' }}">
-       <div class="input-group">
-        <input placeholder="Recherche par nom,  id,  etc..."id="name" type="text" class="form-control" name="recherche" value="{{ old('name') }}" autofocus>
-        <span class="input-group-btn">
+    <form role="form" method="POST" action="{{ url('recherche') }}">
+        {{ csrf_field() }}
+        <div class="form-group{{ $errors->has('recherche') ? ' has-error' : '' }}">
+            <div class="input-group">
+                <input placeholder="Recherche par nom,  id,  etc..." id="name" type="text" class="form-control"
+                       name="recherche" value="{{ old('name') }}" autofocus>
+                <span class="input-group-btn">
                     <button class="btn btn-primary">Recherche</button>
-                </span> 
-       </div> 
-      
-      @if ($errors->has('recherche'))
-      <span class="help-block">
+                </span>
+            </div>
+
+            @if ($errors->has('recherche'))
+                <span class="help-block">
          <strong>{{ $errors->first('recherche') }}</strong>
      </span>
-     @endif
+            @endif
 
- </div>                
-</form>
-@if(isset($results))
-<p>Résultats pour <em>{{$chaine}}</em>:</p>
-    @if(count($results)==0)
-      Aucun résultat. Veuillez réessayer.
-    @else
-        <div class="list-group">
-        @foreach($results as $result) 
-        <a class="list-group-item" href="{{url('dossiers/'.$result->id.'/show')}}">{{$result->prenom.' '.$result->nom}}</a>
-        @endforeach
         </div>
+    </form>
+    @if(isset($results))
+        <p>Résultats pour <em>{{$chaine}}</em>:</p>
+        @if(count($results)==0)
+            Aucun résultat. Veuillez réessayer.
+        @else
+            <div class="list-group">
+                @foreach($results as $result)
+                    <a class="list-group-item"
+                       href="{{url('dossiers/'.$result->id.'/show')}}">{{$result->prenom.' '.$result->nom}}</a>
+                @endforeach
+            </div>
+        @endif
     @endif
-@endif
+    <hr>
+    <div class="panel panel-default col-md-6"><h1>N = {{$dossiers}}</h1></div>
 @endsection
 @section('script')
-<script type="text/javascript">
-</script>
+    <script type="text/javascript">
+    </script>
 @endsection        
