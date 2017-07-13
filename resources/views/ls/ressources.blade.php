46a6234388fce756c72e37c7d2d8226d34e411b5
Francis Lafortune <francislafort@gmail.com>
2017-07-13 14:12:36 -0400

    Modifications liste des ressources

--- resources/views/ls/ressources.blade.php
+++ resources/views/ls/ressources.blade.php
@@ -38,26 +38,39 @@
 
 <div class="col-md-10 col-md-offset-1">
     @if($rep == 'EN')
-    <ul>
-        <li>Personne de confiance de votre entourage (conjoint, collègue, ami)</li>
-        <li>Professionnel d’écoute et de soutien de votre milieu de travail (p.ex. psychologue de l’école, travailleur social, psychoéducateur)</li>
-        <li>Professionnel d’écoute et de soutien du CLSC de votre région (Sherbrooke : 819-563-0144)</li>
-        <li>Professionnel d’écoute et de soutien d’une ligne téléphonique (Tel-Aide Québec 1-877-700-2433).</li>
+        <p>Il se peut que certaines questions vous fassent vivre un malaise, un inconfort ou suscitent une réflexion de votre part.</p>
+        <p>Nous vous invitons à partager ces émotions avec une personne de confiance, dont :</p>
+        <ul>
+        <li>Un membre de votre entourage (conjoint, collègue, ami)</li>
+        <li>Un professionnel d’écoute et de soutien de votre milieu de travail (p.ex. psychologue de l’école, travailleur social, psychoéducateur)</li>
+        <li>Un professionnel d’écoute et de soutien du CLSC de votre région (Sherbrooke : 819-563-0144)</li>
+        <li>Un professionnel d’écoute et de soutien d’une ligne téléphonique (Tel-Aide Québec 1-877-700-2433).</li>
         <li>Pour une aide à plus long terme, vous pouvez consulter le programme d’aide aux employés de votre commission scolaire (CSRS 819-822-5540).</li>
     </ul>
+        <p>Pour une aide à plus long terme, vous pouvez consulter le programme d’aide aux employés de votre commission scolaire (CSRS 819-822-5540).</p>
     @elseif($rep == 'PA')
+        <p>Il se peut que certaines questions vous fassent vivre un malaise, un inconfort ou suscitent une réflexion de votre part.</p>
+        <p>Nous vous invitons à partager ces émotions avec une personne de confiance, dont :</p>
+
         <ul>
-            <li>Personne de confiance de votre entourage (conjoint, collègue, ami)</li>
-            <li>Un membre du personnel de l’équipe du CHUS</li>
-            <li>Professionnel d’écoute et de soutien de votre milieu de travail (p.ex. psychologue, employé des ressources humaines)</li>
-            <li>Professionnel d’écoute et de soutien du CLSC de votre région (Sherbrooke : 819-563-0144)</li>
-            <li>Professionnel d’écoute et de soutien d’une ligne téléphonique (Tel-Aide Québec 1-877-700-2433).</li>
+            <li>Un membre de votre entourage (conjoint, collègue, ami)</li>
+            <li>Un professionnel du module CALME</li>
+            <li>Un professionnel d’écoute et de soutien de votre milieu de travail (p.ex. psychologue, employé des ressources humaines)</li>
+            <li>Un professionnel d’écoute et de soutien du CLSC de votre région (Sherbrooke : 819-563-0144)</li>
+            <li>Un professionnel d’écoute et de soutien d’une ligne téléphonique (Tel-Aide Québec 1-877-700-2433).</li>
         </ul>
     @elseif($rep == 'JE')
+        <p>
+            Il se peut que certaines questions te mettent mal à l’aise, inconfortable ou te portent à réfléchir plus qu’à l’habitude.
+        </p>
+        <p>
+            N'hésite pas à parler des émotions ou des inquiétudes que cela te fait vivre avec une personne de confiance, comme par exemple :
+        </p>
+
         <ul>
-            <li>Personne de confiance de ton entourage (tes parents, frère, sœur, ami)</li>
-            <li>Un membre du personnel de l’équipe du CHUS</li>
-            <li>Professionnel d’écoute et de soutien de ton école (p.ex. psychologue de l’école, travailleur social, psychoéducateur)</li>
+            <li>Personne de ton entourage (tes parents, ton frère, ta sœur, un ami)</li>
+            <li>Un intervenant du module CALME</li>
+            <li>Un intervenant de ton école (p.ex. psychologue de l’école, travailleur social, psychoéducateur)</li>
             <li>La ligne téléphonique Tel-Jeunes (24 heures par jour, 7 jours par semaine, 1-800-263-2266).</li>
         </ul>
     @endif
