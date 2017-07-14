4c01ea212fa2b6a39bc2c3586a780f05e65cbaff
Francis Lafortune <francislafort@gmail.com>
2017-07-14 16:30:49 -0400

    creer widjet prochains suivis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Dossier;
+use App\Mesure;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller {
@@ -16,7 +17,20 @@ class HomeController extends Controller {
     public function index()
     {
        $last = Dossier::all()->sortByDesc('created_at')->take(5);
-        return view('home', compact('last'));
+       $mesures = Mesure::has('dossier')->orderBy('Date')->take(10)->get();
+
+       foreach ($mesures as $mesure) {
+            $mesure['completed'] = $mesure->qCompleted();
+            $is_completed = false;
+            if ($mesure['completed']['deno'] === $mesure['completed']['complet']) {
+                $is_completed = true;
+            }
+            $mesure['is_completed'] = $is_completed;
+       }
+
+       $mesures = $mesures->where('is_completed', '===', false);
+        
+       return view('home', compact('last', 'mesures'));
     }
 
     public function dashbord()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,30 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h1>
+                        Prochains suivis
+                    </h1>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        @foreach($mesures as $mesure)
+                            <a class="list-group-item" href="{{ url('mesures/show', $mesure->id) }}">
+                            {{$mesure->dossier->nom_complet}} -
+                            @if($mesure->temps===1)
+                                Début du traitement: <b>{{$mesure->date->toDateString()}}</b>
+                            @elseif($mesure->temps===2)
+                                Bilan final: <b>{{$mesure->date->toDateString()}}</b>
+                            @endif
+                            <span class="badge">{{$mesure->qCompleted()['complet'].' / '.$mesure->qCompleted()['deno']}}</span>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1>
                         Derniers dossiers créés
                     </h1>
                 </div>
