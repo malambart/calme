7ed6efc34e3cb297e42790cd631f0f98ed2b36f7
Francis Lafortune <francislafort@gmail.com>
2017-08-15 15:41:26 -0400

    ajouté validation date première séance et bilan final

--- app/Http/Controllers/MesuresController.php
+++ app/Http/Controllers/MesuresController.php
@@ -13,30 +13,6 @@ use App\Token;
 use App\Http\Requests;
 
 class mesuresController extends Controller {
-    public function create(Dossier $dossier)
-    {
-        if ($dossier->age < 8) {
-            $this->build($dossier);
-            return back();
-        } else {
-            $ecoles = Ecole::all();
-            return view('mesures.create', compact('dossier', 'ecoles'));
-        }
-
-    }
-
-    public function store(Dossier $dossier, Request $request)
-    {
-        $this->validate($request, [
-            'ecole_id' => 'required',
-            'prenom_ens' => 'required',
-            'nom_ens' => 'required',
-            'tel_ens' => 'required',
-            'courriel_ens' => 'required|email',
-        ]);
-        $this->build($dossier, $request->all());
-        return redirect('/dossiers/show', $dossier->id);
-    }
 
     public function show(Mesure $mesure)
     {
@@ -49,20 +25,6 @@ class mesuresController extends Controller {
         return view('mesures.show', compact('mesure', 'hasEN', 'ete'));
     }
 
-    public function ajoutDate(Mesure $mesure)
-        {
-            return view('mesures/ajoutdate', compact('mesure'));
-        }
-
-    public function storeDate(Mesure $mesure, Request $request)
-        {
-            $this->validate($request,[
-                'date'=>'required|date_format:Y-m-d|after:today'
-                ]);
-            $mesure->date=$request->date;
-            $mesure->save();
-            return redirect(url('dossiers/show',$mesure->dossier->id));
-        }
 
     public function edit(Mesure $mesure)
         {
@@ -71,8 +33,18 @@ class mesuresController extends Controller {
 
     public function update(Mesure $mesure, Request $request)
             {
+                if ($mesure->temps==1) {
+                    $tempsToCompare = 2;
+                    $rule='before:';
+                }
+                elseif ($mesure->temps==2) {
+                    $tempsToCompare = 1;
+                    $rule='after:';
+                }
+
+                $date = Mesure::where('dossier_id', $mesure->dossier_id)->where('temps', $tempsToCompare)->first()->date;
                 $this->validate($request,[
-                    'date'=>'required|date_format:Y-m-d|after:today'
+                    'date'=>'required|date_format:Y-m-d|after:today|'.$rule.$date
                 ]);
                 $mesure->update($request->all());
                 return redirect(url('mesures/show', $mesure->id));
