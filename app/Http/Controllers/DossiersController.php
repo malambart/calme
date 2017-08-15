63118bcdb376ce4ced30e5a021302e72617f1d00
Francis Lafortune <francislafort@gmail.com>
2017-08-15 15:36:41 -0400

    enlevé vieux code inutile

--- app/Http/Controllers/DossiersController.php
+++ app/Http/Controllers/DossiersController.php
@@ -30,8 +30,8 @@ class DossiersController extends Controller
             'sexe' => 'required',
             'no_doss_chus' => 'required|unique:dossiers,no_doss_chus|regex:/^[0-9]{7}$/',
             'date_naiss' => 'required|date_format:Y-m-d|after:1990-01-01',
-            'premiere_seance' => 'required|date_format:Y-m-d|after:today',
-            'bilan_final' => 'required|date_format:Y-m-d|after:today'
+            'premiere_seance' => 'required|date_format:Y-m-d|after:today|before:' . $request->bilan_final,
+            'bilan_final' => 'required|date_format:Y-m-d|after:today|after:' . $request->premiere_seance
         ]);
         $data = $request->all();
         $data['nom_complet'] = $request->prenom . ' ' . $request->nom;
@@ -93,13 +93,15 @@ class DossiersController extends Controller
         return view('dossiers.show', compact('dossier', 'enseignant', 'plan'));
     }
 
-    public function index()
+    public
+    function index()
     {
         $dossiers = Dossier::select(['id', 'nom_complet'])->get();
         return view('dossiers.liste', compact('dossiers'));
     }
 
-    public function recherche(Request $request)
+    public
+    function recherche(Request $request)
     {
         $this->validate($request, ['recherche' => 'required']);
         //dd($request->recherche);
@@ -119,13 +121,15 @@ class DossiersController extends Controller
         return view('recherche', compact('results', 'chaine'));
     }
 
-    public function edit(Dossier $dossier)
+    public
+    function edit(Dossier $dossier)
     {
         return view('dossiers.edit', compact('dossier'));
     }
 
 
-    public function update(Dossier $dossier, Request $request)
+    public
+    function update(Dossier $dossier, Request $request)
     {
         $this->validate($request, [
             'nom' => 'required',
@@ -133,28 +137,6 @@ class DossiersController extends Controller
             'no_doss_chus' => ['required', Rule::unique('dossiers')->ignore($dossier->id), 'regex:/^[0-9]{7}$/'],
             'date_naiss' => 'required|date',
         ]);
-        /*
-        // Si dossier était précédemment exclu,  on créé les token pour les parents et enseignants au besoin...
-        if ($dossier->exclu && !$request->exclu) {
-            $parent = $dossier->getCurrentMesure()->tokens()->where('rep', 'PA')->count();
-            if (!$parent) {
-                $questionnaires = $mesure->questionnaires()->where('temps', $dossier->getCurrentMesure()->temps)->get();
-                // On cree l'invitation dans limeSurvey
-                foreach ($questionnaires as $q) {
-                    if (
-                        ($q->rep != 'JE') |
-                        ($q->rep == 'EN' && ($dossier->premiere_seance->month >= 7 && $dossier->premiere_seance->month < 10))
-                    ) {
-                        continue;
-                    }
-                    $table = env('LS_PREFIX') . 'tokens_' . $q->ls_id;
-                    $token = $mesure->id . $q->ls_id . str_random(12);
-                    DB::connection('ls')->insert('insert into ' . $table . ' (firstname, lastname, token) values (?, ?, ?)', [$mesure->temps, $mesure->id, $token]);
-                    $mesure->tokens()->create(['token' => $token, 'ls_id' => $q->ls_id, 'rep' => $q->rep]);
-                }
-            }
-        }
-        */
 
         $data = $request->all();
         $data['nom_complet'] = $request->prenom . ' ' . $request->nom;
@@ -163,19 +145,22 @@ class DossiersController extends Controller
     }
 
 
-    public function delete(Dossier $dossier)
+    public
+    function delete(Dossier $dossier)
     {
         $dossier->delete();
         return redirect('/');
     }
 
-    public function supprimes()
+    public
+    function supprimes()
     {
         $dossiers = Dossier::onlyTrashed()->select(['id', 'nom_complet'])->get();
         return view('dossiers.supprimes', compact('dossiers'));
     }
 
-    public function restore($dossier)
+    public
+    function restore($dossier)
     {
         $d = Dossier::onlyTrashed()->findOrFail($dossier);
         $d->restore();
