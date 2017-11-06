4d43a09b436a0ecb4739b033d331749a40fd4138
vagrant <vagrant@homestead>
2017-11-06 16:53:17 +0000

    modifie requete home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Dossier;
 use App\Mesure;
@@ -15,8 +16,12 @@ class HomeController extends Controller {
      */
     public function index()
     {
+       //$today1 = today();
+       $today2 = new Carbon();
+       dd($today2);
        $last = Dossier::all()->sortByDesc('created_at')->take(5);
-       $mesures = Mesure::has('dossier')->where('Date', '>=', today())->orderBy('Date')->take(10)->get();
+       //$mesures = Mesure::has('dossier')->where('Date', '>=', $today)->orderBy('Date')->take(10)->get();
+       $mesures = Mesure::has('dossier')->orderBy('Date')->take(10)->get();
        $mesures = $mesures->where('completed', '===', false);
 
        return view('home', compact('last', 'mesures'));
